6cbe291f087a9f9c7d5e3796f4971ae12950ce8e
VinCheetah <vincentgardies03@gmail.com>
2025-01-22 14:44:24 +0100

    cleaner

--- jdot.py
+++ jdot.py
@@ -2,17 +2,8 @@ import numpy as np
 from scipy.spatial.distance import cdist
 import ot
 
-# X: source domain
-# y: source labeks
-# Xtest: target domain
-# ytest is optionnal, just to measure performances of the method along iterations
-# gamma: RBF kernel param (default=1)
-# numIterBCD: number of Iterations for BCD (default=10)
-# alpha: ponderation between ground cost + function cost
-# method: choice of algorithm for transport computation (default: emd)
 
-
-def jdot_nn_l2(get_model, X, Y, Xtest, ytest=[], fit_params={}, reset_model=True, numIterBCD=10, alpha=1, method="emd", reg=1, nb_epoch=100, batch_size=10):
+def jdot_nn_l2(get_model, X, Y, Xtest, ytest=[], fit_params={}, reset_model=True, numIterBCD=10, alpha=1, method="emd", reg=1):
     """
     JDOT with neural network and l2 loss
 
@@ -21,12 +12,12 @@ def jdot_nn_l2(get_model, X, Y, Xtest, ytest=[], fit_params={}, reset_model=True
         X (list): Data from source domain
         Y (list): Labels from source domain
         Xtest (list): Data from target domain
-        ytest (list, optional): Labels from target domain. Defaults to [].
+        ytest (list, optional): Labels from target domain. Is used just to measure the performances fo the method along iterations. Defaults to [].
         fit_params (dict, optional): _description_. Defaults to {}.
         reset_model (bool, optional): Boolean to reset the model at each iteration. Defaults to True.
         numIterBCD (int, optional): Number of Iterations for BCD. Defaults to 10.
         alpha (int, optional): Ponderation between ground cost + function cost. Defaults to 1.
-        method (str, optional): Choice of algorithm for transport computation. Defaults to "emd".
+        method (str, optional): Choice of algorithm for transport computation. Methods avalaible are: "emd", "sinkhorn". Defaults to "emd".
         reg (int, optional): Parameter for sinkhorn. Defaults to 1.
 
     Returns:
@@ -52,7 +43,7 @@ def jdot_nn_l2(get_model, X, Y, Xtest, ytest=[], fit_params={}, reset_model=True
 
     results = {}
 
-    # Init initial g(.)
+    # Init initial model
     model.fit(X, Y, **fit_params)
     ypred = model.predict(Xtest)
 
@@ -61,17 +52,17 @@ def jdot_nn_l2(get_model, X, Y, Xtest, ytest=[], fit_params={}, reset_model=True
     # do it only if the final labels were given
     if len(ytest):
         ydec = np.argmax(ypred, 1) + 1
-        TBR1 = np.mean(ytest == ydec)
-        TBR.append(TBR1)
+        TBR.append(np.mean(ytest == ydec))
 
-    k = 0
-    while k < numIterBCD:  # and not changeLabels:
-        k = k + 1
-        if method == "sinkhorn":
-            G = ot.sinkhorn(wa, wb, C, reg)
-        if method == "emd":
-            G = ot.emd(wa, wb, C)
-
+    for num_iter in range(numIterBCD):
+        match method:
+            case "sinkhorn":
+                G = ot.sinkhorn(wa, wb, C, reg)
+            case "emd":
+                G = ot.emd(wa, wb, C)
+            case _:
+                raise ValueError("Method not implemented")
+            
         Yst = ntest * G.T.dot(Y)
         if reset_model:
             model = get_model()
@@ -83,21 +74,21 @@ def jdot_nn_l2(get_model, X, Y, Xtest, ytest=[], fit_params={}, reset_model=True
         fcost = cdist(Y, ypred, metric="sqeuclidean")
         C = alpha * C0 + fcost
 
-        ydec_tmp = np.argmax(ypred, 1) + 1
-        if k > 1:
+        ydec = np.argmax(ypred, 1) + 1
+        if num_iter > 1:
             sav_fcost.append(np.sum(G * fcost))
             sav_totalcost.append(np.sum(G * (alpha * C0 + fcost)))
 
-        ydec = ydec_tmp
         if len(ytest):
-            TBR1 = np.mean((ytest - ypred) ** 2)
-            TBR.append(TBR1)
+            TBR.append(np.mean((ytest - ypred) ** 2))
 
-    results["ypred0"] = ypred
-    results["ypred"] = np.argmax(ypred, 1) + 1
+    results = {
+        "ypred0": ypred,
+        "ypred": np.argmax(ypred, 1) + 1,
+        "clf": model,
+        "fcost": sav_fcost,
+        "totalcost": sav_totalcost,
+    }
     if len(ytest):
         results["mse"] = TBR
-    results["clf"] = model
-    results["fcost"] = sav_fcost
-    results["totalcost"] = sav_totalcost
     return model, results
